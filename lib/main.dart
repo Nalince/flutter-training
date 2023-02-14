b6793949a4bda6c380aef452679337b2ad542ddf
Nalinhsenid <nalin@hsenidoutsourcing.com>
2023-02-14 17:45:38 +0530

    Add Widgets to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(home: Text('Hello!'),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My first App"),
+        ),
+        body: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: <Widget>[
+            const Text('The Question'),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
